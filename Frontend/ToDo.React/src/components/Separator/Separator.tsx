d4ac1b0623ad55fe8c4546c82e7b9e6d082becaf
Pikatoise <wierwolf@inbox.ru>
2023-11-03 19:27:58 +0500

    start folders

--- Frontend/ToDo.React/src/components/Separator/Separator.tsx
+++ Frontend/ToDo.React/src/components/Separator/Separator.tsx
@@ -1,19 +1,20 @@
 import { FC } from 'react';
 
 export enum Orientation {
-    Vertical = "min-h-max border-e-2",
-    Horizontal = "min-w-max border-b-2"
+    Vertical = "min-h-max border-e-",
+    Horizontal = "min-w-max border-b-"
 }
 
 interface SeparatorProps {
     orientation: Orientation,
     color?: string,
-    margin?: number;
+    margin?: string,
+    stroke?: string;
 }
 
-const Separator: FC<SeparatorProps> = ({ orientation, color = "border-zinc-500", margin = 0, ...props }) => {
+const Separator: FC<SeparatorProps> = ({ orientation, color = "border-zinc-500", margin = "0", stroke = "2" }) => {
     const styles = [
-        orientation,
+        orientation + stroke,
         orientation === Orientation.Horizontal ? `mx-${margin}` : `my-${margin}`,
         color
     ].join(' ');
--- Frontend/ToDo.React/src/components/SidePanel/SidePanel.tsx
+++ Frontend/ToDo.React/src/components/SidePanel/SidePanel.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import BurgerMenu from "../BurgerMenu/BurgerMenu";
 import { Button } from "../ui/button";
-import { LogOut } from "lucide-react";
+import { LogOut, UserSquare2 } from "lucide-react";
 import Separator, { Orientation } from "../Separator/Separator";
 import useAuth from "@/hooks/useAuth";
 import styles from "@/styles/SidePanel.module.css";
@@ -17,20 +17,37 @@ const SidePanel: FC = () => {
         });
     };
 
+    const ProfileClick = () => {
+
+    };
+
     return (
         <BurgerMenu>
             <div className={styles.sidePanel}>
                 <div className={styles.head}>
-                    <div className={styles.login}>
-                        {auth?.user?.Login}
+                    <div className={styles.user}>
+                        <Button size="icon" className={styles.btnProfile} onClick={ProfileClick}>
+                            <UserSquare2 color="#000" strokeWidth={2} className={styles.icon} />
+                        </Button>
+
+                        <div className={styles.login}>
+                            {auth?.user?.Login}
+                        </div>
                     </div>
 
                     <Button size="icon" className={styles.btnExit} onClick={ExitClick}>
-                        <LogOut color="#000" strokeWidth={3} className={styles.icon} />
+                        <LogOut color="#000" strokeWidth={2} className={styles.icon} />
+                        Выйти
                     </Button>
                 </div>
 
-                <Separator orientation={Orientation.Horizontal} margin={2} />
+                <Separator orientation={Orientation.Horizontal} stroke="2" />
+
+                <div className={styles.folders}>
+                    <div>
+                        Folder 1
+                    </div>
+                </div>
             </div>
         </BurgerMenu>
     );
